488d3a85e39e851f93e784f9db1e5b139c6d5362
YeungKaHo <yjh1@meitu.com>
2017-12-23 01:20:45 +0800

    use AudioKit 4.0.4

---
+++ Frameworks/AudioKit.framework/Versions/A/Headers/AKChowningReverbAudioUnit.h
@@ -0,0 +1,15 @@
+//
+//  AKChowningReverbAudioUnit.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka, revision history on Github.
+//  Copyright © 2017 Aurelius Prochazka. All rights reserved.
+//
+
+#pragma once
+
+#import "AKAudioUnitBase.h"
+
+@interface AKChowningReverbAudioUnit : AKAudioUnitBase
+
+@end
---
+++ Frameworks/AudioKit.framework/Versions/A/Headers/AKVariableDelayAudioUnit.h
@@ -0,0 +1,16 @@
+//
+//  AKVariableDelayAudioUnit.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka, revision history on Github.
+//  Copyright © 2017 Aurelius Prochazka. All rights reserved.
+//
+
+#pragma once
+#import "AKAudioUnit.h"
+
+@interface AKVariableDelayAudioUnit : AKAudioUnit
+- (void)clear;
+@property (nonatomic) float time;
+@property (nonatomic) float feedback;
+@end
